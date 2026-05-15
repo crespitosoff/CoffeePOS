12f9f65722b655d5f09db42faec7eba7d33a05b8
crespitosoff <177492567+crespitosoff@users.noreply.github.com>
2026-05-15 00:25:17 -0500

    Fuerza creacion de tablas en arranque

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
-
+from app.models import db  
 app = create_app()
-
+with app.app_context():
+    db.create_all()
+    print("Tablas verificadas/creadas en la base de datos.")
 if __name__ == '__main__':
     app.run(debug=True, port=5000)

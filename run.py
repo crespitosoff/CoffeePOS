3eae6932d2fb2b01243424026730911ed520c746
crespitosoff <177492567+crespitosoff@users.noreply.github.com>
2026-05-15 01:18:12 -0500

    Eliminar admin manual para permitir seeding

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
-
+from app import db, create_app
+from app.models.domain import User
 app = create_app()
 
 if __name__ == '__main__':
+    user_to_delete = User.query.filter_by(username='admin').first()
+    if user_to_delete:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        print("Usuario 'admin' manual eliminado de la base de datos.")
     app.run(debug=True, port=5000)
